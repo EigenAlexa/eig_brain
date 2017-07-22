5473b61dd24a7df92060c7d56addde0a4d716932
JamesMBartlett <james@ml.berkeley.edu>
2017-07-22 14:04:13 -0700

    monkey patch

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
+import sys
 from flask import Flask, request
-from pymongo import MongoClient
+from gevent import monkey
+def stub(*args, **kwargs):
+    pass
+monkey.patch_all = stub
+import grequests
 import requests
 import re
-import grequests
 from eig_state.state_manager import StateManager
-
 app = Flask("eig-brain")
 
 
 @app.route("/", methods=["POST"])
 def handleRequest():
+    print("received request", file=sys.stderr)
     text = request.form['text']
     userid = request.form['userid']
     convid = request.form['convid']
+    sessionid = request.form['sessionid']
 
-    sm = StateManager(userid, convid).next_round(text)
-    if sm.conv_history.state.has_swear:
+    sm = StateManager(userid, convid, sessionid).next_round(text)
+    if sm.conv_history.state_list[-1].has_swear:
         response = "I am not comfortable talking about that. Can we talk about something else?"
-    elif sm.conv_history.state.asks_advice:
+    elif sm.conv_history.state_list[-1].asks_advice:
         response = "I don't think that I have the requisite knowledge to answer such a question."
     else:
         response = discriminate(text)
@@ -27,8 +32,9 @@ def handleRequest():
 def discriminate(text):
     try:
         reqs = [
-          grequests.post('https://3ss5b0g2q4.execute-api.us-east-1.amazonaws.com/production', data={'text':text}),
-          grequests.get('http://107.22.159.20', params={'q': text} )
+          grequests.post('https://3ss5b0g2q4.execute-api.us-east-1.amazonaws.com/production',
+                         data={'text':text}, timeout=2),
+          grequests.get('http://107.22.159.20', params={'q': text}, timeout=3 )
         ]
         aiml_res, goog_res = grequests.map(reqs)
         if goog_res and goog_res.text != "" and goog_res.text != 'Hello' and goog_res.text != 'Query not found':
@@ -36,9 +42,12 @@ def discriminate(text):
         elif aiml_res:
             return aiml_res.text
         else:
+            print("google response", goog_res)
+            print("aiml response", aiml_res)
             return "Could you repeat that, please?"
     except requests.exceptions.RequestException:
         return "Could you repeat that, please?"
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8081)
+    app.run()
+    #app.run(debug=True, host="0.0.0.0", port=8081)
